8982145b31f78793cda509788b789aa1d10167be
Feafly <409435808@qq.com>
2022-02-27 17:33:50 +0800

    Add main logic of ThreadPool

--- ThreadPool/ThreadPool.cpp
+++ ThreadPool/ThreadPool.cpp
@@ -2,7 +2,21 @@
 
 ThreadPool::ThreadPool(const size_t pool_size) : _pool_size(pool_size)
 {
-    // todo
+}
+
+ThreadPool::~ThreadPool()
+{
+    // 关闭线程池
+    std::unique_lock<std::mutex> lock(_mtx_tasks);
+    _is_run = false;
+    lock.unlock();
+    _condition.notify_all();
+
+    // 等待剩余线程执行完毕
+    for (auto &i : _workers)
+    {
+        i.join();
+    }
 }
 
 size_t ThreadPool::size() const
@@ -10,9 +24,70 @@ size_t ThreadPool::size() const
     return _pool_size;
 }
 
+void ThreadPool::start()
+{
+    for (size_t i = 0; i < _pool_size; ++i)
+    {
+        _workers.emplace_back(
+            [this]()
+            {
+                while (this->_is_run)
+                {
+                    // 对任务队列加锁
+                    std::unique_lock<std::mutex> lock(this->_mtx_tasks);
+                    // 如果任务队列一直为空，则等待
+                    // 如果后面函数返回true，则停止等待
+                    while (this->_is_run && this->_tasks.empty())
+                    {
+                        this->_condition.wait(lock,
+                                              [this]()
+                                              {
+                                                  return !this->_is_run;
+                                              });
+                    }
+
+                    // 如果离开循环且队列为空
+                    // 则说明_is_run为false
+                    // 返回，结束线程
+                    if (this->_tasks.empty())
+                    {
+                        return;
+                    }
+
+                    // 取出一个任务
+                    std::function<void()> oneTask = this->_tasks.front();
+                    this->_tasks.pop();
+                    // 执行任务
+                    oneTask();
+                }
+            });
+    }
+}
+
 template <class F, class... Args>
 std::future<typename std::result_of<F(Args...)>::type>
-ThreadPool::enqueue(F &&func, Args &&...args)
+ThreadPool::addTask(F &&func, Args &&...args)
 {
-    // todo
+    // 包裹用户传进来的函数
+    using RtnType = typename std::result_of<F(Args...)>::type;
+    std::packaged_task<RtnType()> usrTask(
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+
+    // 准备包裹返回值
+    std::future<RtnType> res = usrTask.get_future();
+
+    // 添加到队列
+    std::unique_lock<std::mutex> lock(_mtx_tasks);
+    _tasks.emplace(
+        [task = std::move(usrTask)]()
+        {
+            task();
+        });
+    lock.unlock();
+
+    // 通知等待的线程
+    _condition.notify_one();
+
+    // 返回包裹返回值的future
+    return res;
 }
--- ThreadPool/ThreadPool.h
+++ ThreadPool/ThreadPool.h
@@ -13,19 +13,21 @@ class ThreadPool
 public:
     explicit ThreadPool(const size_t pool_size = 0);
 
-    ~ThreadPool() = default;
+    ~ThreadPool();
 
     size_t size() const;
 
-    // enqueue函数接受的参数转发一个函数和该函数的参数
-    // enqueue函数返回一个std::feature，一个将来的值
+    void start();
+
+    // addTask函数接受的参数转发一个函数和该函数的参数
+    // addTask函数返回一个std::feature，一个将来的值
     // std::feature保存的值的类型为函数的返回值的类型
     template <class F, class... Args>
     std::future<typename std::result_of<F(Args...)>::type>
-    enqueue(F &&func, Args &&...args);
+    addTask(F &&func, Args &&...args);
 
 private:
-    // 需要跟踪线程以便加入它们
+    // 执行队列
     std::vector<std::thread> _workers;
     // 任务队列
     std::queue<std::function<void()>> _tasks;
@@ -33,7 +35,7 @@ private:
     const size_t _pool_size = 0;
 
     // 同步
-    std::mutex _queue_mutex;            // 互斥量
+    std::mutex _mtx_tasks;              // 互斥量
     std::condition_variable _condition; // 条件变量
-    bool _stop = false;
+    bool _is_run = true;
 };
