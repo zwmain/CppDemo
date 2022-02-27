fdb4ffc5b578b49e27736e73c2e2b95c633ac28c
Feafly <409435808@qq.com>
2022-02-27 13:58:43 +0800

    packaged_task

--- ThreadStudy/1.5.PackagetaskAsync/main.cpp
+++ ThreadStudy/1.5.PackagetaskAsync/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <future>
+#include <functional>
 
 int task(int a, int b)
 {
@@ -12,9 +13,22 @@ int task(int a, int b)
 
 int main()
 {
-    std::future<int> res = std::async(task, 2, 3);
+    // std::future<int> res = std::async(task, 2, 3);
 
-    std::cout << "结果：" << res.get() << std::endl;
+    // std::cout << "结果：" << res.get() << std::endl;
+
+    std::packaged_task<int(int, int)> t(task);
+
+    std::future<int> f = t.get_future();
+
+    // 如果packaged_task构造的时候仅仅传入原始函数
+    // 那么他可以向普通函数一样被传入thread中
+    // 注意这里必须用std::move，否则会报错
+    std::thread t1(std::move(t), 2, 3);
+
+    std::cout << "结果" << f.get() << std::endl;
+
+    t1.join();
 
     return 0;
 }
