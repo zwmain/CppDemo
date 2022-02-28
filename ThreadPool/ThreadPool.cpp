e3d00b60569b49e4f8a5d21b272e6848d608b5ba
Feafly <409435808@qq.com>
2022-02-28 20:26:34 +0800

    Change the logic of thread loop

--- ThreadPool/ThreadPool.cpp
+++ ThreadPool/ThreadPool.cpp
@@ -31,25 +31,20 @@ void ThreadPool::start()
         _workers.emplace_back(
             [this]()
             {
-                while (this->_is_run)
+                while (true)
                 {
                     // 对任务队列加锁
                     std::unique_lock<std::mutex> lock(this->_mtx_tasks);
                     // 如果任务队列一直为空，则等待
                     // 如果后面函数返回true，则停止等待
-                    while (this->_is_run && this->_tasks.empty())
-                    {
-                        this->_condition.wait(lock,
-                                              [this]()
-                                              {
-                                                  return !this->_is_run;
-                                              });
-                    }
+                    this->_condition.wait(lock,
+                                          [this]()
+                                          {
+                                              return !this->_is_run || !this->_tasks.empty();
+                                          });
 
-                    // 如果离开循环且队列为空
-                    // 则说明_is_run为false
-                    // 返回，结束线程
-                    if (this->_tasks.empty())
+                    // 等待结束后，如果线程被关闭或队列为空，则结束线程
+                    if (!this->_is_run || this->_tasks.empty())
                     {
                         return;
                     }
@@ -57,6 +52,8 @@ void ThreadPool::start()
                     // 取出一个任务
                     std::function<void()> oneTask = this->_tasks.front();
                     this->_tasks.pop();
+                    lock.unlock();
+
                     // 执行任务
                     oneTask();
                 }
