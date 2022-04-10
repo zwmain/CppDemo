58e7dd973bcb169ce569de78159353a589e4cdf3
Feafly <409435808@qq.com>
2022-04-10 17:06:21 +0800

    Set thread pool size with system thread size

--- ThreadPool/main.cpp
+++ ThreadPool/main.cpp
@@ -3,18 +3,19 @@
 
 int test(int a)
 {
-    return a * a;
+    return 2 * a;
 }
 
 int main()
 {
-    ThreadPool pool(4);
-    std::cout << pool.size() << std::endl;
+    size_t sys_thread_size = std::thread::hardware_concurrency();
+    ThreadPool pool(sys_thread_size);
+    std::cout << "线程池大小：" << pool.size() << std::endl;
 
     pool.start();
 
     std::vector<std::future<int>> resVec;
-    for (int i = 0; i < 16; ++i)
+    for (int i = 0; i < 32; ++i)
     {
         auto res = pool.addTask(test, i);
         resVec.push_back(std::move(res));
