16c9cf4b946ca1cdd272bcf24440ce6e4569cc7f
Feafly <409435808@qq.com>
2022-04-28 21:57:59 +0800

    Add annotation for MyUtils splitString

--- MyUtils/MyUtils.hpp
+++ MyUtils/MyUtils.hpp
@@ -1,6 +1,13 @@
 #include <string>
 #include <vector>
 
+/**
+ * @brief 分割字符串
+ *
+ * @param src_str 待分割的字符串
+ * @param separator 分隔字符串
+ * @return 字符串数组
+ */
 std::vector<std::string> splitString(const std::string &src_str, const std::string &separator);
 
 // ----------------------------------------------------------------------------
@@ -8,23 +15,29 @@ std::vector<std::string> splitString(const std::string &src_str, const std::stri
 std::vector<std::string> splitString(const std::string &src_str, const std::string &separator)
 {
     std::vector<std::string> str_arr;
+    // 如果传入字符串为空，直接返回空数组
     if (src_str.empty())
     {
         return str_arr;
     }
+    // 记录上一次搜索的位置，最初为0
     size_t last_pos = 0;
     while (true)
     {
+        // 在字符串中从last_pos位置开始查找分隔符的位置
         size_t cur_pos = src_str.find(separator, last_pos);
         if (std::string::npos == cur_pos)
         {
+            // 如果返回npos说明已经查找不到分隔字符串了
             str_arr.push_back(src_str.substr(last_pos));
             break;
         }
         else
         {
+            // 从上次查找的位置开始，截取字符串
             str_arr.push_back(src_str.substr(last_pos, cur_pos - last_pos));
         }
+        // 跳过分隔字符串长度
         last_pos = cur_pos + separator.size();
     }
     return str_arr;
