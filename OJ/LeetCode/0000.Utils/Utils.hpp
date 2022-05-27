46909de25a764ef39181e5609ace4e19959aa76b
Feafly <409435808@qq.com>
2022-05-27 22:23:04 +0800

    Add LeetCode utils

---
+++ OJ/LeetCode/0000.Utils/Utils.hpp
@@ -0,0 +1,18 @@
+// 刷题辅助工具库
+#ifndef _UTILS_HPP_
+#define _UTILS_HPP_
+
+namespace zwn
+{
+    #include "../../../MyUtils/MyUtils.hpp"
+
+    std::vector<std::vector<int>> inputIntArr(const std::string& fp);
+
+    std::vector<std::vector<int>> inputIntArr(const std::string& fp)
+    {
+        // todo
+    }
+
+} // namespace zwn
+
+#endif
