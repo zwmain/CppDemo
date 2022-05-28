b887685ab7365e826d15b4eddc6ca270c10acbab
Feafly <409435808@qq.com>
2022-05-28 18:57:24 +0800

    Impl Utils

--- OJ/LeetCode/0000.Utils/Utils.hpp
+++ OJ/LeetCode/0000.Utils/Utils.hpp
@@ -2,16 +2,33 @@
 #ifndef _UTILS_HPP_
 #define _UTILS_HPP_
 
-namespace zwn
+#include "../../../MyUtils/MyUtils.hpp"
+
+namespace zwn {
+
+/**
+ * @brief 从文件中读取数组
+ *
+ * @param fp 文件路径
+ * @return 整数数组
+ */
+std::vector<std::vector<int>> inputIntArr(const std::string& fp);
+
+std::vector<std::vector<int>> inputIntArr(const std::string& fp)
 {
-    #include "../../../MyUtils/MyUtils.hpp"
-
-    std::vector<std::vector<int>> inputIntArr(const std::string& fp);
-
-    std::vector<std::vector<int>> inputIntArr(const std::string& fp)
-    {
-        // auto lines=readFileAsLine(fp);
+    std::vector<std::vector<int>> res;
+    auto lines = readFileAsLine(fp);
+    for (auto& line : lines) {
+        auto arr = splitString(line, ",");
+        std::vector<int> num_arr;
+        for (auto& num : arr) {
+            int val = std::stoi(num);
+            num_arr.push_back(val);
+        }
+        res.push_back(std::move(num_arr));
     }
+    return res;
+}
 
 } // namespace zwn
 
