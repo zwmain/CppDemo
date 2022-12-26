6448919ff58e552e6ef208834eef0b24dd90c06e
Feafly <409435808@qq.com>
2022-12-26 16:01:10 +0800

    Format code

--- SocketStudy/01.SocketBase/client/main.cpp
+++ SocketStudy/01.SocketBase/client/main.cpp
@@ -1,18 +1,17 @@
-#include <iostream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
 #include <arpa/inet.h>
 #include <cstring>
+#include <iostream>
+#include <netinet/in.h>
 #include <string>
+#include <sys/socket.h>
+#include <unistd.h>
 
 int main()
 {
     std::cout << "Socket客户端" << std::endl;
 
     int client_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (-1 == client_fd)
-    {
+    if (-1 == client_fd) {
         std::cout << "socket()错误" << std::endl;
         return 0;
     }
@@ -23,9 +22,8 @@ int main()
     server_addr.sin_port = htons(8848);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    int rtn_status = connect(client_fd, (sockaddr *)&server_addr, sizeof(server_addr));
-    if (0 > rtn_status)
-    {
+    int rtn_status = connect(client_fd, (sockaddr*)&server_addr, sizeof(server_addr));
+    if (0 > rtn_status) {
         std::cout << "connect()错误" << std::endl;
         return 0;
     }
@@ -34,17 +32,15 @@ int main()
     size_t data_len = str.size();
     rtn_status = send(client_fd, &data_len, sizeof(data_len), 0);
     rtn_status = send(client_fd, str.c_str(), str.length(), 0);
-    if (0 > rtn_status)
-    {
+    if (0 > rtn_status) {
         std::cout << "send()错误" << std::endl;
         close(client_fd);
         return 0;
     }
 
-    char buffer[1024] = {'\0'};
+    char buffer[1024] = { '\0' };
     int num_recv = recv(client_fd, buffer, sizeof(buffer), 0);
-    if (num_recv > 0)
-    {
+    if (num_recv > 0) {
         str.clear();
         str.append(buffer, num_recv);
         std::cout << str << std::endl;
--- SocketStudy/01.SocketBase/server/main.cpp
+++ SocketStudy/01.SocketBase/server/main.cpp
@@ -1,17 +1,16 @@
-#include <iostream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
 #include <cstring>
+#include <iostream>
+#include <netinet/in.h>
 #include <string>
+#include <sys/socket.h>
+#include <unistd.h>
 
 int main()
 {
     std::cout << "Socket服务端" << std::endl;
 
     int serv_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (-1 == serv_fd)
-    {
+    if (-1 == serv_fd) {
         std::cout << "socket()错误" << std::endl;
         return 0;
     }
@@ -21,54 +20,44 @@ int main()
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(8848);
 
-    int rtn_status = bind(serv_fd, (sockaddr *)&servaddr, sizeof(servaddr));
-    if (0 != rtn_status)
-    {
+    int rtn_status = bind(serv_fd, (sockaddr*)&servaddr, sizeof(servaddr));
+    if (0 != rtn_status) {
         std::cout << "bind()错误" << std::endl;
         return 0;
     }
 
     rtn_status = listen(serv_fd, 5);
-    if (0 != rtn_status)
-    {
+    if (0 != rtn_status) {
         std::cout << "listen()错误" << std::endl;
         return 0;
     }
 
     bool run_flag = true;
-    while (run_flag)
-    {
+    while (run_flag) {
         sockaddr_in client_addr;
         size_t addr_len = sizeof(client_addr);
-        int client_fd = accept(serv_fd, (sockaddr *)&client_addr, (socklen_t *)&addr_len);
-        if (-1 == client_fd)
-        {
+        int client_fd = accept(serv_fd, (sockaddr*)&client_addr, (socklen_t*)&addr_len);
+        if (-1 == client_fd) {
             std::cout << "accept()错误" << std::endl;
             break;
         }
 
-        char buffer[1024] = {'\0'};
+        char buffer[1024] = { '\0' };
         std::string str;
         std::cout << "开始接收数据" << std::endl;
         size_t data_len = 0;
         recv(client_fd, &data_len, sizeof(data_len), 0);
-        while (data_len > 0)
-        {
+        while (data_len > 0) {
             std::memset(buffer, 0, sizeof(buffer));
             int num_recv = recv(client_fd, buffer, sizeof(buffer), 0);
             data_len -= num_recv;
-            if (num_recv == 0)
-            {
+            if (num_recv == 0) {
                 std::cout << "连接断开" << std::endl;
                 break;
-            }
-            else if (num_recv < 0)
-            {
+            } else if (num_recv < 0) {
                 std::cout << "连接错误" << std::endl;
                 break;
-            }
-            else
-            {
+            } else {
                 str.append(buffer, num_recv);
             }
         }
