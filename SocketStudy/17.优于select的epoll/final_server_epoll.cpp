ac3e2d2e2a591f4139bf0928b7cf4805a8ac176f
zwmain <409435808@qq.com>
2023-01-16 23:06:34 +0800

    Finish chapter17 code

---
+++ SocketStudy/17.优于select的epoll/final_server_epoll.cpp
@@ -0,0 +1,139 @@
+#include <arpa/inet.h>
+#include <cstring>
+#include <errno.h>
+#include <fcntl.h>
+#include <iostream>
+#include <string>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+constexpr size_t BUF_SIZE = 4;
+constexpr size_t EPOLL_SIZE = 64;
+
+// 封装函数，设置套接字非阻塞
+void setNoBlockingMode(int fd)
+{
+    int flag = fcntl(fd, F_GETFL);
+    fcntl(fd, F_SETFL, flag | O_NONBLOCK);
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0] << " port" << std::endl;
+        return 0;
+    }
+
+    // 创建套接字
+    int servSock = socket(PF_INET, SOCK_STREAM, 0);
+    if (servSock == -1) {
+        std::cout << "socket 错误" << std::endl;
+        return 0;
+    }
+
+    // 初始化地址信息
+    sockaddr_in servAdr;
+    std::memset(&servAdr, 0, sizeof(servAdr));
+    servAdr.sin_family = AF_INET;
+    servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAdr.sin_port = htons(std::atoi(argv[1]));
+
+    // 绑定地址信息
+    int stu = bind(servSock, (sockaddr*)&servAdr, sizeof(servAdr));
+    if (stu == -1) {
+        close(servSock);
+        std::cout << "bind 错误" << std::endl;
+        return 0;
+    }
+
+    // 进入监听状态
+    stu = listen(servSock, 5);
+    if (stu == -1) {
+        close(servSock);
+        std::cout << "listen 错误" << std::endl;
+        return 0;
+    }
+
+    // 创建epoll
+    int epFd = epoll_create(EPOLL_SIZE);
+    if (epFd == -1) {
+        close(servSock);
+        std::cout << "epoll_create 错误" << std::endl;
+        return 0;
+    }
+
+    // 初始化事件类型
+    epoll_event epEvt;
+    epEvt.events = EPOLLIN | EPOLLET; // 类型指定为EPOLLIN
+    epEvt.data.fd = servSock; // 目标套接字为servSock
+    // 注册epoll事件
+    epoll_ctl(epFd, EPOLL_CTL_ADD, servSock, &epEvt);
+
+    char buf[BUF_SIZE] = { 0 };
+    // 动态创建epoll_event数组，保存发生变化的事件
+    epoll_event* evtArr = new epoll_event[EPOLL_SIZE];
+
+    size_t cntWait = 1;
+    while (true) {
+        // 返回事件数量
+        int epCnt = epoll_wait(epFd, evtArr, EPOLL_SIZE, -1);
+        if (epCnt == -1) {
+            std::cout << "epoll_wait 错误" << std::endl;
+            break;
+        }
+
+        std::cout << "epoll_wait次数：" << cntWait++ << std::endl;
+        // 遍历已触发的事件
+        for (int i = 0; i < epCnt; ++i) {
+            int sockId = evtArr[i].data.fd;
+            // 如果变化的事件套接字等于服务端套接字，说明有新的请求
+            if (sockId == servSock) {
+                sockaddr_in clntAdr;
+                socklen_t szAdr = sizeof(clntAdr);
+                int clntSock = accept(servSock, (sockaddr*)&clntAdr, &szAdr);
+                // 将接收的套接字设置为非阻塞
+                setNoBlockingMode(clntSock);
+                // 注册接收的套接字到epoll
+                epEvt.events = EPOLLIN | EPOLLET;
+                epEvt.data.fd = clntSock;
+                epoll_ctl(epFd, EPOLL_CTL_ADD, clntSock, &epEvt);
+
+                std::string adrStr = inet_ntoa(clntAdr.sin_addr);
+                std::cout << "接收" << adrStr << "的请求：" << clntSock << std::endl;
+            } else {
+                // 读取循环
+                while (true) {
+                    int strLen = read(sockId, buf, BUF_SIZE);
+                    if (strLen == 0) {
+                        // 说明客户端断开连接
+                        epoll_ctl(epFd, EPOLL_CTL_DEL, sockId, nullptr);
+                        close(sockId);
+                        std::cout << "客户端：" << sockId << "断开连接" << std::endl;
+                        break;
+                    } else if (strLen < 0) {
+                        if (errno == EAGAIN) {
+                            // 发生错误且错误值为EAGAIN时，说明读取完毕
+                            std::cout << "套接字：" << sockId << "读取完毕" << std::endl;
+                        } else {
+                            std::cout << "套接字：" << sockId << "读取错误" << std::endl;
+                        }
+                        break;
+                    } else {
+                        buf[strLen] = { 0 };
+                        std::cout << "接收客户端" << sockId << "的数据：" << buf << std::endl;
+                        write(sockId, buf, strLen);
+                    }
+                }
+            }
+        }
+    }
+
+    close(epFd);
+    close(servSock);
+
+    delete[] evtArr;
+
+    return 0;
+}
+
