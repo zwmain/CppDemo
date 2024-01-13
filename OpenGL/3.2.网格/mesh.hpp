3bb05fc5c66b464ffdc53060f00fa6c23eff658c
Feafly <409435808@qq.com>
2024-01-13 21:48:33 +0800

    Add 3.3.Model

--- OpenGL/3.2.网格/mesh.hpp
+++ OpenGL/3.2.网格/mesh.hpp
@@ -6,8 +6,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <shader.h>
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
 
 #include <iostream>
 #include <string>
